e5f3a7a0dd30a852c1d59e88e96fd04e6d398093
alex <alexjfuhr@gmail.com>
2023-11-06 08:42:19 -0500

    Added basic layout

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,18 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <v-app>
+    <v-navigation-drawer>
+      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item link title="List Item 1"></v-list-item>
+      <v-list-item link title="List Item 2"></v-list-item>
+      <v-list-item link title="List Item 3"></v-list-item>
+    </v-navigation-drawer>
+    <v-main>
+      <RouterView/>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
