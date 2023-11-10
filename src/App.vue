83939b1a88f2c18ec80094c007978c6cd3684d9e
alex <alexjfuhr@gmail.com>
2023-11-10 08:50:45 -0500

    Updated navigation and Added views

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const menu = ref([
+  { 
+    path: '/',
+    title: 'About Me'
+  },
+  { 
+    path: '/blog',
+    title: 'Blog'
+  },
+  { 
+    path: '/resume',
+    title: 'Resume'
+  },
+  { 
+    path: '/linkedin',
+    title: 'LinkedIn'
+  },
+  { 
+    path: '/github',
+    title: 'GitHub'
+  }
+])
 </script>
 
 <template>
   <v-app>
     <v-navigation-drawer>
-      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
-      <v-divider></v-divider>
-      <v-list-item link title="List Item 1"></v-list-item>
-      <v-list-item link title="List Item 2"></v-list-item>
-      <v-list-item link title="List Item 3"></v-list-item>
+      <v-list-item title="alexjf.dev" subtitle="Welcome to my website!" />
+      <v-divider />
+      <v-list-item
+        v-for="(item, i) in menu"
+        :key="i"
+        :to="item.path"
+        :title="item.title"
+      ></v-list-item>
     </v-navigation-drawer>
     <v-main>
       <RouterView/>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TestView from '../views/TestView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'test',
-      component: TestView
+      name: 'about-me',
+      component: AboutView
     },
-    {}
+    {
+      path: '/blog',
+      name: 'blog',
+      component: () => import('../views/BlogView.vue')
+    },
+    {
+      path: '/resume',
+      name: 'resume',
+      component: () => import('../views/ResumeView.vue')
+    }
   ]
 })
 
