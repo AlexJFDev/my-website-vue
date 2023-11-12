484ecfdc5b8e6b82edb22c41e4633a2feecf5ba5
alex <alexjfuhr@gmail.com>
2023-11-12 16:06:45 -0500

    Updated about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,5 +2,18 @@
 </script>
 
 <template>
-  Content
+  <div class="text-h3">
+    Alex Fuhrig
+  </div>
+  <div class="text-body">
+    <div>Welcome to my website!</div>
+    <div>I am a young developer who is passionate about Computer Science.</div>
+    <div>Currently, I am enrolled as a Captain at <a href="https://cnu.edu" target="_blank">Christopher Newport University</a>.</div>
+  </div>
 </template>
+
+<style>
+.text-h3 {
+  color: #5b5bff;
+}
+</style>
