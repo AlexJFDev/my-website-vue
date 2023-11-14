33ba0243ac52c900818bbf9e04beb8cf1c671e99
alex <alexjfuhr@gmail.com>
2023-11-13 22:55:22 -0500

    Updated blog data

--- src/blogData.js
+++ src/blogData.js
@@ -1,18 +1,12 @@
 import { reactive } from "vue";
 
 export const blogData = reactive({
-    'blog1': {
-        title: 'blog 1',
-        subtitle: 'blah blah',
-        content: '# blog 1',
-        path: 'blog1',
-        date: '01/01/0001'
-    },
-    'blog2': {
-        title: 'blog 2',
-        subtitle: 'blah blah',
-        content: '# blog 2',
-        path: 'blog2',
-        date: '12/31/9999'
+    'what-is-capstone': {
+        title: 'What is Capstone',
+        subtitle: 'Capstone is a special program for',
+        content: `# What is Capstone
+Capstone is a special program for seniors at my high school. Each student has an internship, job, or some other kind of training experience. Students do their Capstone every day after school. For my Capstone I am working in the IT department at a local research institution.
+Part of the curriculum requires me to write about my experiences or answer a prompt every month so I am posting these writings here.`,
+        date: '9/30/22'
     }
 })
