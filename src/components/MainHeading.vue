466da302398e3c9acb0050e9e99a34bdffe841a7
alex <alexjfuhr@gmail.com>
2023-11-13 23:59:00 -0500

    Added main heading component

---
+++ src/components/MainHeading.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="text-h3 heading-container">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.heading-container {
+  color: #5b5bff;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,9 @@
 <script setup>
+import MainHeading from '../components/MainHeading.vue';
 </script>
 
 <template>
-  <div class="text-h3">
-    Alex Fuhrig
-  </div>
+  <MainHeading>Alex Fuhrig</MainHeading>
   <div class="text-body">
     <div>Welcome to my website! I'm still developing it so please check back often.</div>
     <div>I am a young developer who is passionate about Computer Science.</div>
@@ -13,7 +12,4 @@
 </template>
 
 <style>
-.text-h3 {
-  color: #5b5bff;
-}
 </style>
