0ac641acc5ec262f520c75fd5f16dee709a28821
alex <alexjfuhr@gmail.com>
2023-11-13 22:12:18 -0500

    Updated about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,7 +6,7 @@
     Alex Fuhrig
   </div>
   <div class="text-body">
-    <div>Welcome to my website!</div>
+    <div>Welcome to my website! I'm still developing it so please check back often.</div>
     <div>I am a young developer who is passionate about Computer Science.</div>
     <div>Currently, I am enrolled as a Captain at <a href="https://cnu.edu" target="_blank">Christopher Newport University</a>.</div>
   </div>
