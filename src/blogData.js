82d3e26718705c2331e4b41db71de08a90fa3147
alex <alexjfuhr@gmail.com>
2023-11-13 21:43:30 -0500

    Updated blog view

--- src/blogData.js
+++ src/blogData.js
@@ -4,14 +4,14 @@ export const blogData = reactive({
     'blog1': {
         title: 'blog 1',
         subtitle: 'blah blah',
-        text: 'blog 1',
+        content: '# blog 1',
         path: 'blog1',
         date: '01/01/0001'
     },
     'blog2': {
         title: 'blog 2',
         subtitle: 'blah blah',
-        text: 'blog 2',
+        content: '# blog 2',
         path: 'blog2',
         date: '12/31/9999'
     }
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,7 +1,22 @@
 <script setup>
 import MarkdownBlock from '../components/MarkdownBlock.vue';
+import { blogData } from '../blogData';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute()
+
+/* const blogKey = computed(() => {
+  return route.path.split('/').pop()
+})*/
+
+const blogKey = route.path.split('/').pop()
+
+const blogContent = computed(() => {
+  return blogData[blogKey].content
+})
 </script>
 
 <template>
-  <MarkdownBlock rawMarkdown="# heading"></MarkdownBlock>
+  <MarkdownBlock :rawMarkdown="blogContent"></MarkdownBlock>
 </template>
