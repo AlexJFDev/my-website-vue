dd166a328244e4623d686eed24a0d78cb96c8631
alex <alexjfuhr@gmail.com>
2025-06-29 16:04:56 -0400

    Updated blog dates to follow precise format

--- src/blogData.js
+++ src/blogData.js
@@ -7,7 +7,7 @@ export const blogData = reactive({
         title: 'title',
         subtitle: 'subtitle',
         content: `# Content`,
-        date: '1/1/0001',
+        date: '01/1/0001',
         tags: 'tag'
     },*/
     'what-is-capstone': {
@@ -16,7 +16,7 @@ export const blogData = reactive({
         content: `# What is Capstone
 Capstone is a special program for seniors at my high school. Each student has an internship, job, or some other kind of training experience. Students do their Capstone every day after school. For my Capstone I am working in the IT department at a local research institution.  
 Part of the curriculum requires me to write about my experiences or answer a prompt every month so I am posting these writings here.`,
-        date: '9/30/22',
+        date: '09/30/2022',
         tags: ['capstone', 'school', 'high-school']
     },
     'december-capstone': {
@@ -26,7 +26,7 @@ Part of the curriculum requires me to write about my experiences or answer a pro
 This month I was asked to write about the technical skills that I've used at my Capstone.  
 So far, I have had a lot of programming to do. In particular I have been writing powershell scripts to automate computer setup. I learned about scripting in my 10th grade class when we learned Python. The syntax for Powershell is more primitive but logically it works the same as python. Both languages are also object oriented.  
 More generally I've used my skills in algorithms as well. I wrote an algorithm in JavaScript for a webpage and have made algorithms for my PowerShell scripts too. The JavaScript algorithm was pretty simple but the PowerShell one was more advanced. The Powershell script takes a JSON file and uses it to update registry entries. It uses conditional statements, guard clauses, and for loops.`,
-        date: '12/14/22',
+        date: '12/14/2022',
         tags: ['capstone', 'school', 'high-school']
     },
     'november-capstone': {
@@ -37,7 +37,7 @@ For November I was asked to write about challenges I have encountered in my Caps
 Overall my Capstone has gone pretty smoothly and I haven't encountered any major obstacles. But, I have run into small problems with projects that I've been assigned. So far this year I have been assigned to inspect conference rooms every week, design a new configuration for computers in conference rooms, and update web pages.  
 The first issue I ran into was during my conference room inspections. The microphone on one of the computers I looked at was not working. When some basic troubleshooting didn't fix it I let my manager know. A few days later, my manager and I checked the problem out together. The microphone still wasn't working. Eventually, my manager found the microphone in a cabinet at the back of the room. Someone had unplugged it and put it there.  
 This experience taught me more about troubleshooting. I learned what to do when I can't solve a problem and that it is okay to ask my manager for help.`,
-        date: '11/30/22',
+        date: '11/30/2022',
         tags: ['capstone', 'school', 'high-school']
     },
     'october-capstone': {
@@ -47,7 +47,7 @@ This experience taught me more about troubleshooting. I learned what to do when 
 This month I was asked to write about what I am doing to have a successful experience and who is supporting me.  
 My main support at my Capstone organization is my supervisor. He has done the onboarding process with me and he is meant to be my first contact for any questions or issues that I have. Secondary support might be my dad; he also works at my Capstone organization but is in a different department.  
 To be successful in my Capstone I am trying to show initiative. One way I can demonstrate initiative is by asking lots of questions. This shows my interest and that I am paying attention to what I am being told. I can showcase my skills by speaking up when I am familiar with a concept and doing tasks on my own without needing guidance. This could apply to my skills in programming, networking, and use.`,
-        date: '10/25/22',
+        date: '10/25/2022',
         tags: ['capstone', 'school', 'high-school']
     },
     'altair-assembler': {
@@ -60,7 +60,7 @@ After being introduced to the Altair and writing bytecode for it I was intereste
 The Assembler takes text files as input and outputs binary files. I like to give the input files the extension .alt but you don't have to. The programs use the mnemonics found in the manual for instructions. Arguments for instructions are separated by commas. If the arguments will go bytes separate from the instruction they are in Hex otherwise they are binary. The MOV instruction is different from any other and uses a,b,c,d,e,h,l for arguments. Lastly, dat is a special instruction used for data. Dat takes any number of hex arguments.  
 ## Technical details
 The assembler reads input files one line at a time and one instruction is allowed per line. It starts by finding the instruction name, which should be at the start, and then looks that instruction up in a dictionary (hash table). If the instruction isn't found an error is thrown and the program stops. After this the arguments are found and then passed into a method for the instruction to generate its bytecode. When compilation is finished all the bytes are saved into an output file.`,
-        date: '9/14/2022',
+        date: '09/14/2022',
         tags: ['computer-science', 'school', 'high-school']
     },
     'anagram-algorithms': {
@@ -128,7 +128,7 @@ The Altair uses an eight bit binary input. Since writing out eight digits for ev
 For example, the binary instruction 10011011 is represented as 223 in octal. In normal decimal it is 155.  
 Instead of using Octal you could also use Hexadecimal to represent Altair instructions. In my opinion this is better since two Hexadecimal digits, representing four bits each fit neatly into the Altair's eight bit instructions the only problem with using Hexadecimal is that the Altair's manual uses Octal meaning that you'll have to convert from Octal to Hex frequently.  
 The previous example, 10011011, is represented as 9B.`,
-        date: '9/1/2021',
+        date: '09/1/2021',
         tags: ['computer-science', 'school', 'high-school', 'python']
     },
     'saving-data': {
@@ -174,7 +174,7 @@ I also have some different plants from what I have last year. Last year I plante
 ![My compost area. This is new from last year.](https://i.imgur.com/6L3MGnV.jpeg "My compost area. This is new from last year.")
 #### Testing old seeds. From all these different kinds of seeds only one was good.
 ![Testing old seeds. From all these different kinds of seeds only one was good.](https://i.imgur.com/cLj6HJG.jpeg "Testing old seeds. From all these different kinds of seeds only one was good.")`,
-        date: '7/22/2022',
+        date: '07/22/2022',
         tags: ['gardening', 'outdoors', 'hobbies']
     },
     'java-dates': {
@@ -220,7 +220,7 @@ Fast forward to this week when I regained interest in the project. The first thi
 After coming up with a new name I began tackling the bugs and incomplete features that I had previously filed as issues. The first problem to be solved (and with the least hassle) was the one that I had been having the most difficulty with previously. Within the first day or so of starting work back up on my mod I had fixed the issue. I slowly worked my way through the remaining bugs and then began updating textures, adding more post blocks (you'll understand if you check out the mod), and attempting to get my Cantaloupes to generate naturally.  
 Generating my Cantaloupes proved to be the most difficult issue to solve. I first tried adding the "features" (a technical Minecraft term) in code following along with the tutorial on the Fabric website. Eventually I realized that this tutorial was out of date and I was told to try adding my Cantaloupes a different way with something called a Datapack. But, this had it's own issues first requiring me to update my Minecraft version (a hassle) and then it still didn't work. Eventually after reading the wiki page about Datapacks I realized that I was missing another file but it still didn't work. I resolved to work on solving other problems until eventually I was left with just the Cantaloupes. With fresh eyes I realized that there was a second file that was missing from my Datapack. Once I included this file (actually 3 files) everything worked properly and I built version 0.0.1-beta of my mod.  
 My mod can be found at [this link](https://www.curseforge.com/minecraft/mc-mods/homegrown) if you'd like to check it out.`,
-        date: '7/15/2022',
+        date: '07/15/2022',
         tags: ['computer-science', 'hobbies', 'minecraft', 'java']
     },
     'minecraft-modding-part-two': {
@@ -235,7 +235,7 @@ This first trait, being two blocks tall with extra special properties, was the h
 Firstly it has to be sensitive to the block above it when the crop is planted. It must be air and stay that way until the corn grows taller. My first attempt at implementing this used the wrong method (whenPlanted) instead of (canPlaceAt) this caused crashes because of conditions under which the game calls the methods. After solving this problem I started to work on the crop growth.  
 Initially, once the top half sprouted from the bottom the two halves grew separately. To solve this problem I made it so that the top half would not receive random ticks (with the ticksRandomly method) and instead would grow by checking the age of the lower half every time it got a block update. Basically, this keeps the two halves of the corn the same age.  
 Overall, implementing corn was much easier than implementing tomatoes. That's probably because of the code I was able to copy and the fact that I have more experience modding now. The most difficult part might have actually been deciding where corn should come from. Corn is descended from Teosinte, a plant native to Central Mexico. There's not really an environment similar to this in Minecraft so I spent a while thinking about it. Eventually I decided that that the Badlands would be the best biome for the seeds to come from and that is where it can be gotten now.`,
-        date: '7/19/2022',
+        date: '07/19/2022',
         tags: ['computer-science', 'hobbies', 'minecraft', 'java']
     },
     'goals-for-2024': {
@@ -261,7 +261,7 @@ This goal should be easy if I can successfully manage my time. I just need to se
 ### Finishing my Website
 This has been my longest-running goal on my list, especially posting weekly blogs. Making weekly blogs has been something I have wanted to do for over a year and a half and hopefully, I can make it happen this year.
 Unfortunately, this goal is off to a rocky start. This blog post is intended to be for January 7th, but I'm writing it on January 13th.`,
-        date: '1/07/2024',
+        date: '01/07/2024',
         tags: ['school', 'college', 'personal-development']
     },
     'starting-school-2024': {
@@ -272,7 +272,7 @@ I started my second semester of school at Christopher Newport University on Mond
 Socially, things have been going well too. I have seen most of my friends from last semester, but I'm still trying to find a solid friend group. I think I need to improve my confidence a little more, but that is completely achievable.
 Lastly, I've done a decent job of achieving my goals since coming back to campus. I've been to the gym every day except when I had lacrosse practice and I am not snacking like I was at home. I have been planning my time every day and have done a decent job of executing that plan. Unfortunately, I haven't read every day and I'm not sleeping much better. 
 Overall, I think it's been a pretty good start to the semester.`,
-        date: '1/14/2024',
+        date: '01/14/2024',
         tags: ['school', 'college']
     },
     'getting-organized': {
@@ -290,7 +290,7 @@ Each calendar also has a number at the start of its name. Google calendar sorts 
 ## Files
 I try to organize my files around a central folder. On systems with a UI; I use the desktop, on systems without a UI; I usually use the user folder, and in Google Drive I use the root folder. I try to keep these top level folders free of files and have a folder for everything. I am good about this rule everywhere except Google Drive.  
 I also follow the Capitalized-Kebab-Case naming convention for my files except when I'm using something, like Java or Python, which has a different standard. This is because this naming convention is rarely used by the software and programming languages that I am familiar with so it easily lets me differentiate between what is usually code and documents, photos, or something else.`,
-        date: '1/28/2024',
+        date: '01/28/2024',
         tags: ['personal-development']
     }
 })
