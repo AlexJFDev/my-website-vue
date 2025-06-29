2b7bc77f7611ff4d9c5d09788393374554a290d4
alex <alexjfuhr@gmail.com>
2025-06-29 16:57:55 -0400

    Added DateLabel

---
+++ src/components/DateLabel.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  date: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="text-overline border rounded border-opacity-100">
+    {{ date }}
+  </div>
+</template>
