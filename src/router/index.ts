3a16bbb95633ec3037948f563d95b3ac76f37892
alexjfdev <alexjfuhr@gmail.com>
2026-03-31 18:27:45 -0400

    Fixed router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AboutView from '../views/AboutView.vue'
-import { blogData } from '../blogData'
+import { useBlogStore } from '@/stores/blogs'
+
+const blogStore = useBlogStore()
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -38,7 +40,7 @@ router.beforeEach((to, from) => {
   if (splitPath[1] === '') {
     document.title = 'Welcome'
   } else if (splitPath[1] === 'blog') {
-    document.title = blogData[splitPath[2]]?.title ?? 'Blog'
+    document.title = blogStore.blogs[splitPath[2]]?.title ?? 'Blog'
   } else if (splitPath[1] === 'resume') {
     document.title = 'Resume'
   } else {
